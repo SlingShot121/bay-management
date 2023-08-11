bb6c1a7f72d71dbcd0744bfe4ee3a31e0b74f59c
SlingShot121 <lightningnite843@gmail.com>
2023-08-11 07:01:46 +0200

    1

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('camera-menu').addEventListener('click', function () {
+    document.getElementById('myModal').style.display = 'block';
+});
+
+document.getElementById('closeModal').addEventListener('click', function () {
+    document.getElementById('myModal').style.display = 'none';
+});
