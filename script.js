de973ee404c3b82180b1d1fb45629b0521f984c5
SlingShot121 <lightningnite843@gmail.com>
2023-08-11 14:23:25 +0200

    1

--- script.js
+++ script.js
@@ -6,58 +6,36 @@ document.getElementById('closeModal').addEventListener('click', function () {
     document.getElementById('myModal').style.display = 'none';
 });
 
-document.getElementById('go-button').addEventListener('click', function() {
+document.getElementById('go-button').addEventListener('click', function () {
     let outbay2Element = document.getElementById('outbay-2');
-    if (outbay2Element) {
-        outbay2Element.style.display = 'inline-flex'; // Show the bay-footer element
-    }
     let outbayElement = document.getElementById('outbay');
-    if (outbayElement) {
-        outbayElement.style.display = 'inline-flex'; // Show the bay-footer element
-    }
+    if (outbay2Element) outbay2Element.style.display = 'inline-flex';
+    if (outbayElement) outbayElement.style.display = 'inline-flex';
 });
 
-document.getElementById('add-button').addEventListener('click', function() {
+document.getElementById('add-button').addEventListener('click', function () {
     let outbay2Element = document.getElementById('outbay-2');
-    if (outbay2Element) {
-        outbay2Element.style.display = 'none'; // Show the bay-footer element
-    }
     let outbayElement = document.getElementById('outbay');
-    if (outbayElement) {
-        outbayElement.style.display = 'none'; // Show the bay-footer element
-    }
-});
+    if (outbay2Element) outbay2Element.style.display = 'none';
+    if (outbayElement) outbayElement.style.display = 'none';
 
-document.getElementById('add-button').addEventListener('click', function() {
     for (let i = 47; i <= 57; i++) {
         let element = document.querySelector('.div-div-' + i);
-        if (element) {
-            element.style.display = 'inline-flex'; // Show the element
-        }
+        if (element) element.style.display = 'inline-flex';
     }
     let bayFooterElement = document.getElementById('bay-footer');
-    if (bayFooterElement) {
-        bayFooterElement.style.display = 'inline-flex'; // Show the bay-footer element
-    }
     let bayCapElement = document.getElementById('bay-cap');
-    if (bayCapElement) {
-        bayCapElement.style.display = 'inline-flex'; // Show the bay-footer element
-    }
+    if (bayFooterElement) bayFooterElement.style.display = 'inline-flex';
+    if (bayCapElement) bayCapElement.style.display = 'inline-flex';
 });
 
-document.getElementById('remove-button').addEventListener('click', function() {
+document.getElementById('remove-button').addEventListener('click', function () {
     for (let i = 47; i <= 57; i++) {
         let element = document.querySelector('.div-div-' + i);
-        if (element) {
-            element.style.display = 'none'; // Hide the element
-        }
+        if (element) element.style.display = 'none';
     }
     let bayFooterElement = document.getElementById('bay-footer');
-    if (bayFooterElement) {
-        bayFooterElement.style.display = 'none'; // Hide the bay-footer element
-    }
     let bayCapElement = document.getElementById('bay-cap');
-    if (bayCapElement) {
-        bayCapElement.style.display = 'none'; // Hide the bay-footer element
-    }
+    if (bayFooterElement) bayFooterElement.style.display = 'none';
+    if (bayCapElement) bayCapElement.style.display = 'none';
 });
