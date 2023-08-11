3d725bf89593ee38ec1eb55fb0afb6040be8b7b3
SlingShot121 <lightningnite843@gmail.com>
2023-08-11 14:06:22 +0200

    1

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ document.getElementById('camera-menu').addEventListener('click', function () {
 document.getElementById('closeModal').addEventListener('click', function () {
     document.getElementById('myModal').style.display = 'none';
 });
+
+document.getElementById('add-button').addEventListener('click', function() {
+    for (let i = 47; i <= 57; i++) {
+        let element = document.querySelector('.div-div-' + i);
+        if (element) {
+            element.style.display = 'block'; // Show the element
+        }
+    }
+});
+
+document.getElementById('remove-button').addEventListener('click', function() {
+    for (let i = 47; i <= 57; i++) {
+        let element = document.querySelector('.div-div-' + i);
+        if (element) {
+            element.style.display = 'none'; // Hide the element
+        }
+    }
+});
